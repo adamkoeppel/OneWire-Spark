fc197284f08ead23de7a80e3cd30ce71e27598ad
adamkoeppel <koeppel@gmail.com>
2015-07-21 11:47:14 -0400

    stripped down more
    address printing and presence sensing

--- rewriting_onewire/rewriting_onewire.ino
+++ rewriting_onewire/rewriting_onewire.ino
@@ -11,7 +11,7 @@
 // 6/2014 - Hotaman
 
 // Define the pins we will use
-int ow = 3;    // put the onewire bus on D0 (or 0 for a regular Arduino)
+int ow = 3;    // put the onewire bus on D3 (or 3 for a regular Arduino)
 
 //create the oneWire instance
 OneWire  oneWire(ow);  // OneWire commands will be prefixed with oneWire
@@ -23,7 +23,7 @@ uint8_t DS2438_address3[] = { 0x26, 0x6D, 0xDD, 0xD6, 0x01, 0x00, 0x00, 0xF9 }; 
 
 
 void setup(void) {
-  Serial.begin(57600);  // local hardware test only, for Spark set to 57600, for conventional set to 9600
+  Serial.begin(9600);  // local hardware test only, for Spark set to 57600, for conventional set to 9600
 
 //put valve configuration here
 
@@ -32,65 +32,16 @@ void setup(void) {
 
 void loop(void) {
   byte i;
-  byte present = 0;
+//  byte present = 0;
   byte type_s = 2;
-  byte data[12];
+  byte data_T[12];
+  byte data_V[12];
   byte addr[8];
   float celsius, fahrenheit;
 
-/*
-  if ( !oneWire.search(addr)) {
-    Serial.println("No more addresses.");
-    Serial.println();
-    oneWire.reset_search();
-    delay(250);
-    return;
-  }
-  
-  Serial.print("ROM =");
-  for( i = 0; i < 8; i++) {
-    Serial.write(' ');
-    Serial.print(addr[i], HEX);
-  }
-
-  if (OneWire::crc8(addr, 7) != addr[7]) {
-      Serial.println("CRC is not valid!");
-      return;
-  }
-  Serial.println();
-
- 
-  // the first ROM byte indicates which chip
-  // includes debug output of chip type
-  switch (addr[0]) {    
-    case 0x10:
-      Serial.println("  Chip = DS18S20");  // or old DS1820
-      type_s = 1;
-      break;
-    case 0x28:
-      Serial.println("  Chip = DS18B20");
-      type_s = 0;
-      break;
-    case 0x22:
-      Serial.println("  Chip = DS1822");
-      type_s = 0;
-      break;      
-    case 0x26:
-      Serial.println("  Chip = DS2438");
-      type_s = 2;
-      break;
-    default:
-      Serial.println("Device is not a DS18x20/DS1822/DS2438 device. Skipping...");
-      return;
-  } 
-*/
-
-
   oneWire.reset();
   oneWire.select(DS2438_address2);      // Just do one at a time for testing
-                        // change to skip if you already have a list of addresses
-                        // then loop through them below for reading
-                        
+
   oneWire.write(0x44);        // start conversion, with parasite power on at the end
   
   delay(2900);     // maybe 750ms is enough, maybe not, I'm shooting for 1 reading per second
@@ -99,48 +50,52 @@ void loop(void) {
                     
   // we might do a oneWire.depower() here, but the reset will take care of it.
   
-  present = oneWire.reset();
+//  present = 
+  oneWire.reset();
   oneWire.select(DS2438_address2);    
   oneWire.write(0xB8,0);         // Recall Memory 0
   oneWire.write(0x00,0);         // Recall Memory 0
 
-  present = oneWire.reset();
+//  present = 
+  oneWire.reset();
   oneWire.select(DS2438_address2);    
   oneWire.write(0xBE, 0);         // Read Scratchpad 0
   oneWire.write(0x00,0);         // Recall Memory 0
 
-  Serial.print("  Data = ");
-  Serial.print(present, HEX);
-  Serial.print(" ");
+
+  Serial.print("  Data T = ");
+//  Serial.print(present, HEX);
+//  Serial.print(" ");
   for ( i = 0; i < 9; i++) {           // we need 9 bytes
-    data[i] = oneWire.read();
-    Serial.print(data[i], HEX);
-    Serial.print(" ");
+    data_T[i] = oneWire.read();
+//    Serial.print(data_T[i], HEX);
+//    Serial.print(" ");
   }
-  Serial.print(" CRC=");
-  Serial.print(OneWire::crc8(data, 8), HEX);
+//  Serial.print(" CRC=");
+//  Serial.print(OneWire::crc8(data_T, 8), HEX);
   Serial.println();
+
 
   // Convert the data to actual temperature
   // because the result is a 16 bit signed integer, it should
   // be stored to an "int16_t" type, which is always 16 bits
   // even when compiled on a 32 bit processor.
-  int16_t raw = (data[1] << 8) | data[0];
+  int16_t raw = (data_T[1] << 8) | data_T[0];
   if (type_s) {
     if (type_s==1) {    // DS18S20
       raw = raw << 3; // 9 bit resolution default
-      if (data[7] == 0x10) {
+      if (data_T[7] == 0x10) {
         // "count remain" gives full 12 bit resolution
-        raw = (raw & 0xFFF0) + 12 - data[6];
+        raw = (raw & 0xFFF0) + 12 - data_T[6];
       }
       celsius = (float)raw / 16.0;
     }else{ // type_s==2 for DS2438
-      if (data[2] > 127) data[2]=0;
-      data[1] = data[1] >> 3;
-      celsius = (float)data[2] + ((float)data[1] * .03125);
+      if (data_T[2] > 127) data_T[2]=0;
+      data_T[1] = data_T[1] >> 3;
+      celsius = (float)data_T[2] + ((float)data_T[1] * .03125);
     }
   } else {  // DS18B20 and DS1822
-    byte cfg = (data[4] & 0x60);
+    byte cfg = (data_T[4] & 0x60);
     // at lower res, the low bits are undefined, so let's zero them
     if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
     else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
